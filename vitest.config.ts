675f473935ad5fb834bc006cc644b5d7bfce9572
zikarimov <zikarimov@mail.ru>
2024-09-13 09:43:15 +0000

    update

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
+    coverage: {
+      provider: "v8",
+      extension: [".tsx"],
+      reporter: ['text', 'json-summary', 'json'],
+    },
   },
-})
+});
